d7999f0b093fda06f6c810e0f9737055d9a7d9c9
Salman <codingwithsalman@gmail.com>
2025-08-22 10:44:51 +0500

    updated project to support 36

--- app/src/main/java/com/codingwithsalman/apps/todo/app/compose/features/note/presentation/MainActivity.kt
+++ app/src/main/java/com/codingwithsalman/apps/todo/app/compose/features/note/presentation/MainActivity.kt
@@ -3,9 +3,13 @@ package com.codingwithsalman.apps.todo.app.compose.features.note.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -21,14 +25,14 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             TodoAppComposeTheme {
-                Surface(
-                    color = MaterialTheme.colors.background
-                ) {
+                Scaffold { paddingValues ->
                     val navController = rememberNavController()
                     NavHost(
+                        modifier = Modifier.padding(paddingValues),
                         navController = navController,
                         startDestination = Screen.NotesScreen.route
                     ) {
